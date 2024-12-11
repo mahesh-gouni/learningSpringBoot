db9586f732f6d29f6e129e22e2c4d94252203072
gouni <gounimahesh9000@gmail.com>
2024-12-11 22:48:24 +0530

    Initial commit

--- learningSpringBoot/src/main/java/com/spring/learningSpringBoot/LearningSpringBootApplication.java
+++ learningSpringBoot/src/main/java/com/spring/learningSpringBoot/LearningSpringBootApplication.java
@@ -1,18 +1,25 @@
 package com.spring.learningSpringBoot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearningSpringBootApplication {
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
-		ApplicationContext context= SpringApplication.run(LearningSpringBootApplication.class, args);
+		//ApplicationContext context=
+				SpringApplication.run(LearningSpringBootApplication.class, args);
 
 
-		Developer developer=context.getBean(Developer.class);
-		developer.greet();
+		//Developer developer=context.getBean(Developer.class);
+		//developer.greet();
 
 	}
 
